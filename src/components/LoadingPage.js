3f2cf281fca4ce235037739d5486dd9caa917713
Victor Lopez <victorel94@gmail.com>
2020-05-31 16:00:26 -0700

    init commit

---
+++ src/components/LoadingPage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const LoadingPage = () => (
+    <div className='loader'>
+        <img
+            className='loader__image'
+            src='/images/loader.gif'>
+        </img>
+    </div>
+
+)
+
+
+export default LoadingPage
